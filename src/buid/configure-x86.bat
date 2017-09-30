383fe9300137053a8750f2de432885660d1145ac
VincentPT <minhpta@outlook.com>
2017-09-30 20:45:49 +0700

    - update build scripts.

--- src/buid/configure-x86.bat
+++ src/buid/configure-x86.bat
@@ -7,7 +7,7 @@ cd %THIS_SCRIPT_PATH%
 
 cmake -G "Visual Studio 14 2015" ^
         -DCINDER_INCLUDE_DIR=E:/Projects/externals/cinder/include -DCINDER_LIB_DIR=E:/Projects/externals/cinder/lib/vc14/x86 ^
-        -DOPENCV_INCLUDE_DIR=E:/working/mkrdteam-hcm/3rdparties/windows/opencv-3.1.0/include -DOPENCV_LIB_DIR=E:/working/mkrdteam-hcm/3rdparties/windows/opencv-3.1.0/vc14/lib/x86 ^
+        -DOPENCV_INCLUDE_DIR=E:/working/trunk/3rdparties/windows/opencv-3.1.0/include -DOPENCV_LIB_DIR=E:/working/trunk/3rdparties/windows/opencv-3.1.0/vc14/lib/x86 ^
         ..
 cd %CURR_DIR%
 
